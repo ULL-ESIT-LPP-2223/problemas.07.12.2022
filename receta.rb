f6cd9985af476202ece619f4c5304a2506044094
Coromoto Leon <cleon@ull.edu.es>
2017-12-05 05:58:15 +0000

    yield

--- receta.rb
+++ receta.rb
@@ -5,6 +5,8 @@ class Recipe
     @name = name
     @ingredients = []
     @instructions = []
+
+    yield self
   end
 
   def to_s
@@ -18,17 +20,31 @@ class Recipe
 
     output
   end
+
+  def ingredient(name, options = {})
+    ingredient = name
+    ingredient << " (#{options[:amount]})" if options[:amount]
+
+    @ingredients << ingredient
+  end
+
+  def step(text, options = {})
+    instruction = text
+    instruction << " (#{options[:during]})" if options[:during]
+
+    @instructions << instruction
+  end
 end
 
-mac_and_cheese = Recipe.new("Mac and Cheese")
+mac_and_cheese = Recipe.new("Noodles and Cheese") do |r| 
+  r.ingredient "Water",   :amount => "2 cups"
+  r.ingredient "Noodles", :amount => "1 cup"
+  r.ingredient "Cheese",  :amount => "1/2 cup"
 
-mac_and_cheese.ingredients << "Noodles"
-mac_and_cheese.ingredients << "Water"
-mac_and_cheese.ingredients << "Cheese"
-
-mac_and_cheese.instructions << "Boil water."
-mac_and_cheese.instructions << "Add noodles, boil for six minutes."
-mac_and_cheese.instructions << "Drain water."
-mac_and_cheese.instructions << "Mix in cheese with noodles."
+  r.step "Heat water to boiling.",        :during => "5 minutes"
+  r.step "Add noodles to boiling water.", :during => "6 minutes"
+  r.step "Drain water."
+  r.step "Mix cheese in with noodles."
+end
 
 puts mac_and_cheese
